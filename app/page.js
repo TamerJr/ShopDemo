19f9fe31072697eb0d4e7ee00714e34787407a23
TamerJr <ahmttmer@gmail.com>
2023-05-15 01:01:50 +0300

    add api file

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import ProdCard from './Components/ProdCard';
+import Image from "next/image";
+import styles from "./page.module.css";
+import ProdCard from "./Components/ProdCard";
 
-const handleGetSample=async()=>{
+const handleGetSample = async () => {
   try {
     const res = await fetch("https://fakestoreapi.com/products?limit=3");
     const data = await res.json();
@@ -12,14 +12,35 @@ const handleGetSample=async()=>{
     alert(err.message);
   }
 };
+const handleGetWatchs = async () => {
+  try {
+    const res = await fetch("https://dummyjson.com/products?limit=10");
+    const data = await res.json();
 
-export default async function  Home() {
-  const homePageData=await handleGetSample()
+    return data;
+  } catch (err) {
+    alert(err.message);
+  }
+};
+export default async function Home() {
+  const homePageData = await handleGetSample();
+  const watchsData = await handleGetWatchs();
+  console.log(watchsData)
   return (
     <main className={styles.main}>
-      {homePageData?.map((prod,index)=>(
-        <ProdCard prod={prod} key={index}/>
-      ))}
+      <section className={styles.SampleProd}>
+        {homePageData?.map((prod, index) => (
+          <ProdCard prod={prod} key={index} />
+        ))}
+      </section>
+      <div className={styles.WatchsBar}>
+        {watchsData?.products?.map((ele) => (
+            <div>
+              <Image src={ele.thumbnail} width={500} height={500} />
+              <p>{ele.title}</p>
+            </div>
+        ))}
+      </div>
     </main>
-  )
+  );
 }
